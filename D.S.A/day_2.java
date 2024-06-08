bfb156dc3efd46a55360765e53c350d1962a5147
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-08 14:01:57 +0530

    1

--- D.S.A/day_2.java
+++ D.S.A/day_2.java
@@ -1,5 +1,53 @@
+
+import java.util.*;
+
 public class day_2 {
     public static void main(String[] args) {
-        
+        // Loops
+        System.out.println("Hello World!");
+
+        // for
+        // for(int counter=0;counter<30;counter++){
+        //     System.out.println("Hello world!");
+        // }
+        // for (int i = 0; i < 11; i++) {
+        //     System.out.println(i);
+        // }
+
+        // While
+        // int i=0;
+        // while (i<11) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // do while
+        // int i=0;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // } while(i<11);
+
+
+        // ques1
+        // Scanner sc=new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int sum=0;
+        // for(int i=1;i<=n;i++){
+        //     sum=sum+i;
+        // }
+        // System.out.println(sum);
+
+        // ques2
+        // Scanner sc =new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int multi=0;
+        // int ans=0;
+        // for(int i=1;i<11;i++){
+        //     ans=n*i;
+        //     // System.out.println(ans);
+        //     multi=multi+1;
+        //     System.out.println(n+" x "+multi+" = "+ans);
+        // }
     }
 }
