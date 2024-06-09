3fe4677443f514abbb03effca2caae21d582bbec
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-10 00:14:26 +0530

    1

--- D.S.A/day_3.java
+++ D.S.A/day_3.java
@@ -36,11 +36,46 @@ public class day_3 {
 
         // Half Pyramid with Numbers
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(j);
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.err.println();
+        // }
+
+        // Invereted Half Pyramid with Numbers
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i+1;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // Floyd's Triangle
+
+        // int x=1;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(x+" ");
+        //         x++;
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+        // 0-1 Triangle
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                int sum=i+j;
+                if (sum%2==0) {
+                    System.out.print("1 ");
+                }else{
+                    System.out.print("0 ");
+                }
             }
-            System.err.println();
+            System.out.println();
         }
 
     }
