7687de05756e3a32acc7f4dc42bc442b51dc66fd
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-09 19:45:07 +0530

    1

---
+++ D.S.A/day_3.java
@@ -0,0 +1,18 @@
+public class day_3 {
+    public static void main(String[] args) {
+
+        int n=5;
+        int m=5;
+
+        // Half Pyramid
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        
+    }
+}
