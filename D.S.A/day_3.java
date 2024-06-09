ef1c9bc6839faac5604082b24aaeddb6a4f2d016
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-09 20:13:25 +0530

    1

--- D.S.A/day_3.java
+++ D.S.A/day_3.java
@@ -13,6 +13,35 @@ public class day_3 {
         //     System.out.println();
         // }
 
-        
+        // Inverted Half Pyramid
+
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Inverted Half Pyramid(180)
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Half Pyramid with Numbers
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.err.println();
+        }
+
     }
 }
