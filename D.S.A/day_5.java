ee05a00273d0d307c474566d4c9cec3cc327ede0
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-25 22:23:29 +0530

    1

---
+++ D.S.A/day_5.java
@@ -0,0 +1,70 @@
+public class day_5 {
+    public static void main(String[] args) {
+
+        int n=5;
+
+        // Butterfly Pattern
+
+        
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces=2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=n;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     int spaces=2*(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Solid Rhombus
+
+        // for(int i=1;i<=n;i++){
+        //     int spaces=(n-i);
+        //     for(int j=1;j<=spaces;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=5;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Number Pyramid
+        
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(i+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Palindromic Pattern
+
+        for(int i=0;i<=n;i++){
+            
+        }
+
+    }
+}
