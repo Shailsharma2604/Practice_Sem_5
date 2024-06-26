5f5339fcbd2fd9fbd1afc970388430d47d5ed17c
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-26 23:50:51 +0530

    1

--- D.S.A/day_6.java
+++ D.S.A/day_6.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class day_6 {
-    public static void main(String[] args) {
-        
-    }
+
+    // // first function
+
+    // public static void printMyName(String name) {
+    //     System.out.println(name);
+    //     return;
+    // }
+    // public static void main(String[] args) {
+    //      Scanner sc=new Scanner(System.in);
+    //      String name =sc.nextLine();
+    //      printMyName(name);
+    // }
+
+
+    // Adding 2 numbers
+
+    // public static void addnumbers(int a,int b){
+    //     System.out.println(a+b);
+    //     return;
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc=new Scanner(System.in);
+    //     int a=sc.nextInt();
+    //     int b=sc.nextInt();
+    //     addnumbers(a, b);
+    // }        
 }
