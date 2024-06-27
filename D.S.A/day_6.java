98bdf015d299d5b78ff0d846f90bd7100cc0bae0
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-27 17:47:40 +0530

    1

--- D.S.A/day_6.java
+++ D.S.A/day_6.java
@@ -26,5 +26,19 @@ public class day_6 {
     //     int a=sc.nextInt();
     //     int b=sc.nextInt();
     //     addnumbers(a, b);
-    // }        
+    // }
+
+
+    // Multipy 2 numbers
+
+    public static void Multipy(int a,int b){
+        System.out.println(a*b);
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        Multipy(a, b);
+    }
 }
