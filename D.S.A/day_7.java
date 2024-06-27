f503bf17817ff24e957f5c28328987cd3bddbc77
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-27 21:20:34 +0530

    1

---
+++ D.S.A/day_7.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class day_7 {
+    public static void main(String[] args) {
+        // Array
+        int[] marks =new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=99;
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
