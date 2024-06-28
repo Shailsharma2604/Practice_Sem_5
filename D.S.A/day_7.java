c44b723fd3e4a1ebd2c528816e7f4edd2f47c16e
Shailsharma2604 <shailsharma020604@gmail.com>
2024-06-28 18:28:23 +0530

    1

--- D.S.A/day_7.java
+++ D.S.A/day_7.java
@@ -4,15 +4,26 @@ import java.util.*;
 public class day_7 {
     public static void main(String[] args) {
         // Array
-        int[] marks =new int[3];
-        marks[0]=97;
-        marks[1]=98;
-        marks[2]=99;
-        // System.out.println(marks[0]);
-        // System.out.println(marks[1]);
-        // System.out.println(marks[2]);
+
+        // // Way_1
+        // int[] marks =new int[3];
+        // marks[0]=97;
+        // marks[1]=98;
+        // marks[2]=99;
+        // // System.out.println(marks[0]);
+        // // System.out.println(marks[1]);
+        // // System.out.println(marks[2]);
 
 
+        // for(int i=0;i<3;i++){
+        //     System.out.println(marks[i]);
+        // }
+
+
+
+        // Way_2
+
+        int[] marks={1,2,3,4,5,6};
         for(int i=0;i<3;i++){
             System.out.println(marks[i]);
         }
