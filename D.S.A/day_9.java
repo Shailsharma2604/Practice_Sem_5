4e354e037a8a7d12f20182cbd88728d540f555cf
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-01 22:44:41 +0530

    1

---
+++ D.S.A/day_9.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class day_9 {
+    public static void main(String[] args) {
+
+        // String Declaration
+
+        // String nsme="Tony";
+        // String fullname="Tony Stark";
+        // String sentence =" my name is Tony Stark";
+
+        // Scanner sc=new Scanner(System.in);
+        // String name=sc.nextLine();
+        // System.out.println("Your name is :- "+name);
+
+
+        // String Declaration
+
+        // String firstname="tony";
+        // String secondname="stark";
+        // String fullname=firstname+" "+secondname;
+        // System.out.println(fullname.length());
+
+        // carAt
+
+        
+    }
+}
