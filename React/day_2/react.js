3d85a965d3be6053d85a10d21b076eb71e5ec4cf
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-07 23:29:27 +0530

    1

--- React/day_2/react.js
+++ React/day_2/react.js
@@ -1,7 +1,12 @@
-function customRender(reactElement)
-
+function customRender(reactElement,container){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    container.appendChild(domElement)
+}
 const reactElement={
-    type:'a';
+    type:'a',
     props:{
         href:'https://www.google.com',
         target:'_blank'
