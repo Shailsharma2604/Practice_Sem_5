fac442d1568ab773a2af43347f9429b4684a8a74
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-13 16:01:06 +0530

    1

---
+++ D.S.A/day_10.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class day_10 {
+    public static void main(String[] args) {
+
+        // Scanner in =new Scanner (System.in);
+        // int [] arr=new int[9];
+        // // System.out.println(rnos[0]);
+
+        // // String[] arr= new String[8];
+        // // System.out.println(arr[0]);
+
+        // for(int i=0;i<arr.length;i++){
+        //     arr[i]=in.nextInt();
+        // }
+        // // for(int i=0;i<arr.length;i++){
+        // //     System.out.print(arr[i]+" ");
+        // // }
+        // for(int num : arr){
+        //     System.out.print(num+" ");
+        // }
+
+        // System.out.println(Arrays.toString(arr));
+
+
+
+
+        
+    }
+}
