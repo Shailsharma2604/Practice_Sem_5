983e8464c5a14c4cd1c758308032af686534555c
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-17 00:33:11 +0530

    1

---
+++ D.S.A/day_13.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class day_13 {
+    // public static void main(String[] args) {
+    //     int[] arr={1,5,6,78,8,9};
+    //     swap(arr,1,3);
+    //     System.out.println(Arrays.toString(arr));
+    // }
+    // static void swap(int[] arr,int index1,int index2){
+    //     int temp=arr[index1];
+    //     arr[index1]=arr[index2];
+    //     arr[index2]=temp;
+    // }
+
+    public static void main(String[] args) {
+        int[] arr= {1,23,45,5,5};
+        System.out.println(max(arr));
+    }
+
+    static int max(int[] arr){
+        
+    }
+    
+}
