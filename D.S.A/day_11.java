b9f2738caf02b37da92d9167078b2b3a93567af3
Shailsharma2604 <shailsharma020604@gmail.com>
2024-07-16 17:04:32 +0530

    1

--- D.S.A/day_11.java
+++ D.S.A/day_11.java
@@ -1,3 +1,15 @@
 public class day_11 {
-    
+    public static void main(String[] args) {
+        int[][]arr={
+            {1,2,3,4},
+            {5,6},
+            {7,8,9}
+        };
+        for(int rows=0;rows<arr.length;rows++){
+            for(int cols=0;cols<arr[rows].length;cols++){
+                System.out.print(arr[rows][cols]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
