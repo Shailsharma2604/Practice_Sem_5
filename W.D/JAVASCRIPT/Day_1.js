0ff0f3f1aee63d63faaf3638edea45739df12485
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-08 14:18:59 +0530

    1

--- W.D/JAVASCRIPT/Day_1.js
+++ W.D/JAVASCRIPT/Day_1.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+// const account_Id = 144553
+// let account_Email = "shail020604@gmail.com"
+// var account_Password = "123456"
+// account_City = "jaipur"
+
+// // account_Id=2
+
+// account_Email = "shail"
+// account_Password = "2121"
+// account_City = "pune"
+
+// console.log(account_Id);
+// console.log(account_Email);
+// console.log(account_Password);
+// console.log(account_City);
+// console.table({ account_Id, account_Email, account_Password, account_City });
+
+
+
+// "use strict";
+  
+// // alert(3+3)
+
+// let name = "shail"
+// let age = 18
+// let isLoggedIn = false
+
+// console.log(typeof "shail")
+// console.log(typeof 15)
+// console.log(typeof true)
+
+
