8c4ccea9f010885d2e377b19afa6a1f9b18c6ed6
Shailsharma2604 <shailsharma020604@gmail.com>
2024-08-31 15:38:12 +0530

    1

--- D.S.A/day_14.java
+++ D.S.A/day_14.java
@@ -1,5 +1,20 @@
+import java.util.LinkedList;
+
 public class day_14 {
     public static void main(String[] args) {
-        
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+
+        LinkedList<Integer> num = new LinkedList<Integer>();
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(4);
+        System.out.println(num);
     }
 }
