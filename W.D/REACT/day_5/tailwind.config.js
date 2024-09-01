2d1f5957bc14b79b33a5c012b92eff106420c135
Shailsharma2604 <shailsharma020604@gmail.com>
2024-09-01 12:42:23 +0530

    1

--- W.D/REACT/day_5/tailwind.config.js
+++ W.D/REACT/day_5/tailwind.config.js
@@ -1,9 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
