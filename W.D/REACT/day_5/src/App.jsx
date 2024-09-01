1da8ef08767624d5354e3e3cee1d5e1f2cdd36de
Shailsharma2604 <shailsharma020604@gmail.com>
2024-09-01 13:59:15 +0530

    1

--- W.D/REACT/day_5/src/App.jsx
+++ W.D/REACT/day_5/src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myobj = {
+    username: "shail",
+    age: 26
+  }
+
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline mb-4">
       Hello world!
     </h1>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Test</h1>
+    <Card channel="shail" someobj={myobj}/>
+    <Card/>
     </>
   )
 }
