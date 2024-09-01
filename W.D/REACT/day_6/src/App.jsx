865ebb7b8a413f5dcd1b1a5983a996bab4363442
Shailsharma2604 <shailsharma020604@gmail.com>
2024-09-01 14:25:14 +0530

    1

--- W.D/REACT/day_6/src/App.jsx
+++ W.D/REACT/day_6/src/App.jsx
@@ -4,30 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color,setcolor] = useState("olive")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h- screen duration-200" style={{backgroundColor: color}}>
+      <div className='fixed flex flex-warp justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-warp justify-center gap-3 shadow-lgbg-white px-3 py-2 rounded-full'>
+          <button className='outline-none px-4' style={{backgroundColor: 'red'}} onClick={()=>setcolor('red')}></button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
