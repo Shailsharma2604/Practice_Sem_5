66da1d3d22acf6df48269aa087e91345b636f067
Shailsharma2604 <shailsharma020604@gmail.com>
2024-09-14 18:48:32 +0530

    1

--- W.D/REACT/day_7/src/App.jsx
+++ W.D/REACT/day_7/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useCallback, useState } from 'react'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  function App(){
+    const[length,setLength] = useState(8)
+    const[numerAllowed,setNumberAllowed] = useState(false)
+    const[charAllowed,setCharAllowed] = useState(false)
+    const[password,setPassword] = useState("")
+    const passwordGenerator = useCallback(()=> {
+      let pass = ""
+      let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz"
+      if (numerAllowed) {
+        str+="0123456789"
+      }
+      if (charAllowed) {
+        str +="!@#$%^&*-_=+(){}[]~`"
+      }
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+      }
+    },[length,numerAllowed,charAllowed,setPassword])
+  }
   return (
     <>
-    <h1>Password genrator</h1>
+    <h1 className = 'text-4xl test-center'>Password genrator</h1>
     </>
   )
 }
