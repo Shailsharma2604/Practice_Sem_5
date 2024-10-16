eccef7aafb1ceaf0b7f4fb8daa9fe3a5b78d0195
Shailsharma2604 <shailsharma020604@gmail.com>
2024-10-16 22:09:56 +0530

    1

--- D.S.A/day_23.java
+++ D.S.A/day_23.java
@@ -1,12 +1,21 @@
 public class day_23 {
     public static void main(String[] args) {
-        int ans=fibo(4);
-        System.out.println(ans);
+        int ans_1=fibo(4);
+        System.out.println(ans_1);
+        print(1);
     }
     static int fibo(int n){
         if(n<2){
             return n;
         }
         return fibo(n-1)+fibo(n-2);
+    }
+    static void print(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print(n+1);
     }
 }
