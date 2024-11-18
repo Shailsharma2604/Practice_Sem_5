3719389ee18e4b12c925aa8b3882c01233fa18ad
Shailsharma2604 <shailsharma020604@gmail.com>
2024-11-18 19:22:49 +0530

    1

--- D.S.A/day_27.java
+++ D.S.A/day_27.java
@@ -1,5 +1,13 @@
 public class day_27 {
     public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,16,8};
+        System.out.println(sorted(arr, 0));
         
+    }
+    static boolean sorted(int[] arr,int index){
+        if(index==arr.length-1){
+            return true;
+        }
+        return arr[index]<arr[index+1] && sorted(arr,index+1);
     }
 }
