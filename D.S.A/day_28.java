7685c36e76e629b13b1f71c0cab5a2e8d65757f4
Shailsharma2604 <shailsharma020604@gmail.com>
2024-12-20 19:17:04 +0530

    1

--- D.S.A/day_28.java
+++ D.S.A/day_28.java
@@ -1,5 +1,25 @@
+
+import java.util.*;
+
 public class day_28 {
     public static void main(String[] args) {
-        
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("Kunal",89);
+        map.put("Karan",99);
+        map.put("Rahul",94);
+
+        System.out.println(map.get("Karan"));
+        System.out.println(map.getOrDefault("Apoorv",78));
+        System.out.println(map.containsKey("Karan"));
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(56);
+        set.add(60);
+        set.add(65);
+        set.add(85);
+        set.add(5);
+
+        System.out.println(set);
     }
 }
