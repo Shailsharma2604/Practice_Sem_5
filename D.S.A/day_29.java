6ca4bb780d487b31b2639f8acb3671999bb692a0
Shailsharma2604 <shailsharma020604@gmail.com>
2025-01-02 22:27:00 +0530

    1

---
+++ D.S.A/day_29.java
@@ -0,0 +1,14 @@
+public class day_29 {
+    public static void main(String[] args) {
+
+        // String[] arr={"a","b","c"};
+        // for(int i=0;i<=arr.length;i++){
+        //     arr[i];
+        // }
+
+        String a="abcdefgh";
+        for(int i=0;i<a.length();i++){
+            System.out.println(a.charAt(i));
+        }
+    }
+}
